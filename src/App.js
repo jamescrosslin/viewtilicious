22633270fac16aa583f35cdb3e73d0ce195eb59f
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-03 11:03:30 -0700

    added SearchBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;

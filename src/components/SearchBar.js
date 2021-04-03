9378357c835d7b8fd66573ef7e6cb40eff803a6e
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-03 09:19:57 -0700

    fleshed out basic SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,16 @@ function SearchBar(props) {
   const [searchText, setSearch] = useState("");
   const { prop } = props;
   return (
-    <form onSubmit={}>
-      <input></input>
-      <svg></svg>
+    <form className="search-form" onSubmit={}>
+      <input
+        type="search"
+        placeholder="Search for an image..."
+        name="search"
+        onChange={() => {}}
+      ></input>
+      <button type="submit">
+        {/* Need to add a magnifying glass buton here or figure out how to do svgs*/}
+      </button>
     </form>
   );
 }

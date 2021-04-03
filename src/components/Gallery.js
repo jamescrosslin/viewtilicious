3bd6bed81d0994303d6e5251acc8f216e6c1b568
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-03 09:10:41 -0700

    created new components for basic app structure

---
+++ src/components/Gallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Photo from "./Photo";
+
+function Gallery(props) {
+  const { prop } = props;
+  return <Photo />;
+}
+
+export default Gallery;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function SearchBar(props) {
+  const [searchText, setSearch] = useState("");
+  const { prop } = props;
+  return (
+    <form onSubmit={}>
+      <input></input>
+      <svg></svg>
+    </form>
+  );
+}
+
+export default SearchBar;

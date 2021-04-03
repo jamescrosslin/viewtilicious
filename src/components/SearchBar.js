25d769fce680d5822c0e8ad6744f108da9bfe9e1
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-03 11:03:06 -0700

    added state and interaction functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 
 function SearchBar(props) {
-  const [searchText, setSearch] = useState("");
-  const { prop } = props;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+    //put callback to send state to App.js here
+    e.currentTarget.reset();
+  };
+
+  const saveText = (e) => {
+    const inputText = e.currentTarget.value;
+    setSearchText(() => inputText);
+  };
+
   return (
-    <form className="search-form" onSubmit={}>
+    <form className="search-form" onSubmit={handleSubmit}>
       <input
         type="search"
         placeholder="Search for an image..."
         name="search"
-        onChange={() => {}}
+        onChange={saveText}
+        required
       ></input>
       <button type="submit" className="submit-button">
         {/* Need to add a magnifying glass buton here or figure out how to do svgs*/}

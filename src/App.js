ec0aa69cdbdab6f2961f2f7ec894d487b6845a58
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-04 14:08:42 -0700

    changed config structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import SearchBar from "./components/SearchBar";
+import apiKey from "../config.js";
 
 function App() {
+  const fetchPics = (genre) => {};
+
   return (
     <div className="App">
       <SearchBar />

5dec72f6589fdfa51a13541ef34365098e57168d
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-04 15:39:35 -0700

    added mock data for gallery

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Nav from "./components/Nav";
 import Gallery from "./components/Gallery";
 import apiKey from "./config.js";
 
+const mockData = [
+  {
+    url: "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
+    alt: "1st image",
+  },
+  {
+    url: "https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg",
+    alt: "2nd image",
+  },
+];
+
 function App() {
   const fetchPics = (genre) => {};
 
@@ -10,7 +21,7 @@ function App() {
     <div className="App">
       <SearchBar />
       <Nav />
-      <Gallery />
+      <Gallery data={""} />
     </div>
   );
 }

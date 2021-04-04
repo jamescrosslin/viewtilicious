6d7eb88e839e849c31bcaaf15cbfa485ece57206
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-04 15:22:34 -0700

    added preliminary map to create photo components

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,8 +2,22 @@ import React from "react";
 import Photo from "./Photo";
 
 function Gallery(props) {
-  const { prop } = props;
-  return <Photo />;
+  const { data } = props;
+  return (
+    <div class="photo-container">
+      <h2>Results</h2>
+      <ul>
+        {data?.length ? (
+          data.map((pic) => <Photo src={pic.url} alt={pic.desc} />)
+        ) : (
+          <li class="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default Gallery;

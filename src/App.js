c05297ab7592f186566b019e4fb7ef4222ac3dcc
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-05 21:21:31 -0700

    working, but jumping

--- src/App.js
+++ src/App.js
@@ -9,36 +9,43 @@ import { Route, Switch, useLocation } from "react-router";
 
 function App() {
   const { pathname } = useLocation();
-  const queryRegex = /^\/(.+)$/gi;
-  const path = queryRegex.test(pathname)
-    ? pathname.replace(queryRegex, "$1")
-    : null;
 
   const [photos, setPhotos] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [query, setQuery] = useState(path);
+  const [query, setQuery] = useState(null);
 
   function handleQuery(text) {
     setQuery(() => text);
+  }
+
+  async function handleFetch(tags) {
+    const {
+      data: {
+        photos: { photo },
+      },
+    } = await axios.get(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&tag_mode=all&per_page=24&format=json&nojsoncallback=1`
+    );
+    return photo;
   }
 
   useEffect(() => {
     async function fetchPics() {
       await setLoading(() => true);
       const searchTerms = query.split(/[^\w\d]/).join("+");
-      const {
-        data: {
-          photos: { photo },
-        },
-      } = await axios.get(
-        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchTerms}&tag_mode=all&per_page=24&format=json&nojsoncallback=1`
-      );
+      const photo = await handleFetch(searchTerms);
       await setPhotos(() => photo);
       await setLoading(() => false);
     }
 
+    const queryRegex = /^\/(.+)$/gi;
+    const path = queryRegex.test(pathname)
+      ? pathname.replace(queryRegex, "$1")
+      : null;
+
+    handleQuery(path);
     if (query) fetchPics();
-  }, [query]);
+  }, [query, pathname]);
 
   return (
     <div className="container">

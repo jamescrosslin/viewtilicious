c2157609a9efe30704b33fbaad5f3520191b1071
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-08 20:59:00 -0700

    added animation to loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Loading() {
-  return <span>Loading...</span>;
+  return (
+    <div className="loading">
+      {`Loading...`.split("").map((letter, i) => (
+        <span
+          className={`letter`}
+          key={i}
+          style={{ animationDelay: `${0.12 * i}s` }}
+        >
+          {letter}
+        </span>
+      ))}
+    </div>
+  );
 }
 
 export default Loading;

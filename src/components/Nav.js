4c8548600db5d93eeeaaed42852d43c0eb6a8156
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-10 11:27:03 -0700

    added form component to change topics

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Nav({ topics }) {
-  return (
-    <nav className="main-nav">
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputs, setInputs] = useState({});
+
+  const handleTopicChange = (topics) => {};
+
+  return isEditing ? (
+    <form>
+      <ul>
+        {topics.map((topic, i) => (
+          <li key={i}>
+            <input defaultValue={topic} />
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => setIsEditing((editing) => !editing)}
+        style={{
+          position: "absolute",
+          top: "5px",
+          right: "5px",
+          backgroundColor: "#438bbd",
+          color: "white",
+          borderRadius: "5px",
+        }}
+      >
+        Done
+      </button>
+    </form>
+  ) : (
+    <nav
+      className="main-nav"
+      style={{ outline: "1px solid black", position: "relative" }}
+    >
       <ul>
         {topics.map((topic, i) => (
           <li key={i}>
@@ -11,8 +42,24 @@ function Nav({ topics }) {
           </li>
         ))}
       </ul>
+      <span
+        onClick={() => setIsEditing((editing) => !editing)}
+        className="hides"
+        style={{
+          position: "absolute",
+          top: "5px",
+          right: "5px",
+          opacity: 0.4,
+        }}
+      >
+        &#x270D;
+      </span>
     </nav>
   );
 }
 
 export default Nav;
+
+{
+  /* */
+}

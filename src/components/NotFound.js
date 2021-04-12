125637e80fcc3d69850dff04879772f002c548e2
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-11 21:19:44 -0700

    added 404 message

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,21 +1,27 @@
 import React from "react";
+import { matchPath, useLocation, useParams, useRouteMatch } from "react-router";
 import notfound from "../img/404.gif";
 
-function NotFound(props) {
-  const { prop } = props;
+function NotFound() {
   return (
-    <figure>
-      <img src={notfound} alt="A woman sitting on top of 404 block letters" />
-      <figcaption>
-        <a
-          href="http://storyset.com/web"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Illustration by Freepik Storyset
-        </a>
-      </figcaption>
-    </figure>
+    <div>
+      <h3>
+        Sorry, we can't find that page. Try the search bar or the preselected
+        topics!
+      </h3>
+      <figure>
+        <img src={notfound} alt="A woman sitting on top of 404 block letters" />
+        <figcaption>
+          <a
+            href="http://storyset.com/web"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Illustration by Freepik Storyset
+          </a>
+        </figcaption>
+      </figure>
+    </div>
   );
 }
 

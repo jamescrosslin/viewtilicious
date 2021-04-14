f5ee9f7c98cd8de26b040c91fc70c24d6a1643b7
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-14 16:57:08 -0700

    added comments

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,15 +3,19 @@ import React from "react";
 function Loading() {
   return (
     <div className="loading">
-      {`Loading...`.split("").map((letter, i) => (
-        <span
-          className={`letter`}
-          key={i}
-          style={{ animationDelay: `${0.12 * i}s` }}
-        >
-          {letter}
-        </span>
-      ))}
+      {
+        // transforms each letter in the loading string to a span
+        `Loading...`.split("").map((letter, i) => (
+          <span
+            className={`letter`}
+            key={i}
+            // linear growth in delays leads to a mechanical wave-like animation
+            style={{ animationDelay: `${0.12 * i}s` }}
+          >
+            {letter}
+          </span>
+        ))
+      }
     </div>
   );
 }

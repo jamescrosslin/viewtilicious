1404ef61077d776aada6e915c215f51fa59e3117
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-14 16:43:38 -0700

    added comments and factored out makePhotos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,39 @@
 import React from "react";
 import Photo from "./Photo";
 
-function Gallery({ data, message }) {
+function Gallery({ data, children }) {
+  /**
+   * @function makePhotos
+   * @param {Array} photoData
+   * @returns {Array} photo components with individual photo data
+   */
+  function makePhotos(photoData) {
+    return photoData.map((pic, i, arr) => (
+      <Photo
+        // delays the entry of components based on index so that a 4-column grid in order from rightmost column to leftmost
+        // in different views with less columns creates a cool checkerboard entrance
+        delay={(4 - (i % 4)) / 10}
+        key={pic.id}
+        {...pic}
+      />
+    ));
+  }
   return (
     <div className="photo-container">
-      <h2>{message}</h2>
+      {children}
       <ul>
-        {data?.length ? (
-          data.map((pic, i, arr) => (
-            <Photo delay={(4 - (i % 4)) / 10} key={pic.id} {...pic} />
-          ))
-        ) : (
-          <li className="not-found">
-            <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
-          </li>
-        )}
+        {
+          // runs makePhotos only when valid data is passed
+          data?.length ? (
+            makePhotos(data)
+          ) : (
+            //if data was searched but has no contents, renders results NoResults component
+            <li className="not-found">
+              <h3>No Results Found</h3>
+              <p>Your search did not return any results. Please try again.</p>
+            </li>
+          )
+        }
       </ul>
     </div>
   );
